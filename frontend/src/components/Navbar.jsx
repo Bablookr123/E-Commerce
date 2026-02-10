0d230631abe9fdd4b169c32a8c4f0c1684bfda95
Bablookr123 <bkumars1521@gmail.com>
2026-02-10 09:06:20 +0530

    updating navbar with search and filtering options

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
@@ -7,7 +7,13 @@ import { assets } from "../assets/assets";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
-  const { user, setUser ,navigate,setShowUserLogin} = useContext(AppContext);
+  const { user, setUser ,navigate,setShowUserLogin,cartCount,searchQuery,setSearchQuery} = useContext(AppContext);
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      navigate("/products");
+    }
+  }, [searchQuery]);
+
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
       <Link to={"/"}>
@@ -19,7 +25,7 @@ const Navbar = () => {
         <Link to={"/products"}>All Products</Link>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-          <input
+          <input onChange={(e)=>setSearchQuery(e.target.value)}
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
@@ -52,8 +58,8 @@ const Navbar = () => {
         <div onClick={() => navigate("/cart")}
         className="relative cursor-pointer">
          <img src={assets.cart_icon} alt="" className="w-6 h-6"/>
-          <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">
-            3
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-4.5 h-4.5 rounded-full">
+            {cartCount()}
           </button>
         </div>
         {user ? (
@@ -102,7 +108,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`${open ? "flex" : "hidden"} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+        className={`${open ? "flex" : "hidden"} absolute top-15 left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
       >
         <Link to={"/"}>Home</Link>
         <Link to={"/products"}>All Products</Link>
