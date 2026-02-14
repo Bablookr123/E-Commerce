8ddac93510f76a19b883bc323715eee93c309789
Bablookr123 <bkumars1521@gmail.com>
2026-02-14 10:30:53 +0530

    configuring seller routes

---
+++ Backend/routes/seller.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { isAuthseller, sellerLogin, sellerLogout } from "../controllers/seller.controller.js";
+import { authSeller } from "../middlewares/authSeller.js";
+
+
+const router = express.Router();
+
+router.post("/login", sellerLogin);
+router.get("/is-auth",authSeller,isAuthseller)
+router.get("/logout",authSeller,sellerLogout)
+
+
+export default router;
